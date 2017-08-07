d8ffd5e73d56968f753cfc8152a0a3199ee4ced2
Peter DeMartini <thepeterdemartini@gmail.com>
2017-08-07 14:53:11 -0700

    v3.0.1 use latest browser-meshblu-http without lodash issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-var path              = require('path');
-var webpack           = require('webpack');
+var path = require("path")
+var webpack = require("webpack")
 
 var plugins = [
   new webpack.NoEmitOnErrorsPlugin(),
   new webpack.optimize.OccurrenceOrderPlugin(),
   new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
+    "process.env": {
+      NODE_ENV: JSON.stringify("production"),
+    },
   }),
 ]
 
 module.exports = {
-  devtool: 'cheap-source-map',
+  devtool: "cheap-source-map",
   node: {
     fs: "empty",
     child_process: "empty",
@@ -21,21 +21,22 @@ module.exports = {
     repl: "empty",
   },
   resolve: {
-    extensions: ['.js', '.coffee']
+    extensions: [".js", ".coffee"],
   },
-  entry: [ './index.coffee' ],
+  entry: ["./index.coffee"],
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-    libraryTarget: 'umd',
-    library: 'MeshbluJsonSchemaResolver'
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+    libraryTarget: "umd",
+    library: "MeshbluJsonSchemaResolver",
   },
   plugins: plugins,
   module: {
     rules: [
       {
-        test: /\.coffee$/, loader: 'coffee-loader'
-      }
-    ]
-  }
-};
+        test: /\.coffee$/,
+        loader: "coffee-loader",
+      },
+    ],
+  },
+}
